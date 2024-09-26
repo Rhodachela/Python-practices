124882f017c48e70e220e1f525128b4bc3f73dfc
Rhoda <rhodachela@gmail.com>
2024-09-26 11:27:40 +0300

    Arithmetic operations functions

---
+++ Functions-Structures/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return("Cannot complete this operation")
+            else:
+                return num1 / num2
+        case _:
+            return("Invalid input. Try again!")
+        
+        
+       
