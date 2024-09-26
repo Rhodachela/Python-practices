55b0b0ef684023eb7a2b4e8cdc607becb7b60b03
Rhoda <rhodachela@gmail.com>
2024-09-26 11:28:20 +0300

    This is the main file

---
+++ Functions-Structures/main.py
@@ -0,0 +1,18 @@
+from arithmetic_operations import perform_operation
+
+def main():  
+    while True:
+        print("Arithmetic operations")
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        operation = str(input("Enter the operation (add, subtract, multiply, divide): ")).strip().lower()
+         
+        if operation == "quit":
+            print("Exiting the program. Goodbye!")
+            break
+        
+        result = perform_operation(num1, num2, operation)
+        print(f"The result is {result}")
+
+if __name__ == "__main__":
+    main()
