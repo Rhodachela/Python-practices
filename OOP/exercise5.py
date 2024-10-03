3972356c6a31b3f393dea3e0f5b643f9f64a98f6
Rhoda <rhodachela@gmail.com>
2024-10-03 12:16:09 +0300

    Custom exception

---
+++ OOP/exercise5.py
@@ -0,0 +1,15 @@
+class ValueTooHighError(Exception):
+    pass
+def check_value(number):
+    if number > 100:
+        raise ValueTooHighError(f"Error: The value {number} is too high. Try again!")
+    else:
+        print(f"The value {number} is within the acceptable range")
+    
+try:
+    number = int(input("Enter number: "))
+    check_value(number)
+except ValueTooHighError as e:
+    print(e)
+except ValueError:
+    print("Invalid input. Enter a valid integer.")
