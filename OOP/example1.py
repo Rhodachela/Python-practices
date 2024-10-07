7280d869fcd987539fe93e7732b5ae59dbcfeb16
Rhoda <rhodachela@gmail.com>
2024-10-07 23:52:18 +0300

    Importing Unittest for testing

--- OOP/example1.py
+++ OOP/example1.py
@@ -4,8 +4,8 @@ def add(x, y):
 
 class TestAdd(unittest.TestCase):
     def test_add_positive(self):
-        result = add(5, 3)
-        self.assertEqual(result, 8)    
+        result = add(6, 9)
+        self.assertEqual(result, 15)    
     def test_add_negative(self):
         result = add(-5, 7)
         self.assertEqual(result, 2)
