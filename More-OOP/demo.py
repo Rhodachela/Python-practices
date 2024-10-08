fa762328b7130ef4011540d8eaa3b3b4bf4689ed
Rhoda <rhodachela@gmail.com>
2024-10-08 13:57:24 +0300

    Understanding classes and methods

--- More-OOP/demo.py
+++ More-OOP/demo.py
@@ -1,19 +1,26 @@
 class A:
+    def __init__(self):
+        print("in A init")
+
     def feature1(self):
-        print("Feature 1 is working")
+        print("Feature 1-A is working")
 
     def feature2(self):
         print("Feature 2 is working")
 
 class B:
+    def __init__(self):
+        print("in B init")
+
     def feature3(self):
-        print("Feature 3 is working")
+        print("Feature 1-B is working")
     def feature4(self):
         print("Feature 4 is working")
 
-a1 = A()
-a1.feature1()
-a1.feature2()
+class C(B, A):
+    def __init__(self):
+        super().__init__()
+        print("in C init")
 
-b1 = B()
-b1.feature3()
+a1 = C()
+a1.feature1()
